d55f44df6ae5b6f87a183a125015161a74348c0e
JamesZheng <jameszheng@mobagel.com>
2023-03-21 01:32:20 +0800

    change data types of inputs from `list` to `dict`

--- QEB/model.py
+++ QEB/model.py
@@ -2,121 +2,196 @@ import numpy as np
 import pandas as pd
 import random
 from scipy.optimize import minimize
-from scipy.stats import multivariate_normal
-from typing import List, Dict, Tuple, Optional
+from typing import List, Dict, Tuple, Union
 
 random.seed(0)
 np.random.seed(0)
 
 
 class QEB:
-    def __init__(
-        self,
-        mu_0: Optional[int] = None,
-        sigma_0: Optional[int] = None,
-        types_of_proteins: Optional[int] = None,
-        numbers_of_each_type: Optional[List] = None,
-        adjustment_list: Optional[List[List]] = None,
-        grid_points: Optional[np.ndarray] = None,
-        variances_of_error: Optional[List] = None,
-        beta_list: Optional[List] = None,
-        epsilon_list: Optional[List[np.ndarray]] = None,
-        data_log_list: Optional[List[np.ndarray]] = None,
-    ) -> None:
-        self.mu_0 = 0 if mu_0 is None else mu_0
-        self.sigma_0 = 1 if sigma_0 is None else sigma_0
-        self.types_of_proteins = 1 if types_of_proteins is None else types_of_proteins
-        self.numbers_of_each_type = [] if numbers_of_each_type is None else numbers_of_each_type
-        self.adjustment_list = [] if adjustment_list is None else adjustment_list
-        self.grid_points = np.array([]) if grid_points is None else grid_points
-        self.variances_of_error = [] if variances_of_error is None else variances_of_error
-        self.beta_list = [] if beta_list is None else beta_list
-        self.epsilon_list = [] if epsilon_list is None else epsilon_list
-        self.data_log_list = [] if data_log_list is None else data_log_list
+    def create_data(
+            self,
+            mu_0: int = 2,
+            sigma_0: int = 0.5,
+            numbers_of_each_type: Dict[str, int] = {"A": 3, "B": 2},
+            adjustment: Dict[str, List[int]] = {"A": [1] * 3, "B": [1.2] * 2},
+            grid_points: np.ndarray = np.repeat(np.array([1, 2, 3]), 20),
+            variances_of_error: Dict[str, List[int]] = {"A": [1] * 3, "B": [1.5] * 2},
+    ) -> Dict[str, np.ndarray]:
+        """
+        Create simulated data with given hyper parameters
 
-        self.X_k = np.array([]) if grid_points is None else - \
-            1 / 2 * grid_points**2
+        Params
+        ----------
+        mu_0: int
+            The `mu` of normal distribution in the first layer of bayesian
+        sigma_0: int =0.5
+            The `sigma` of normal distribution in the first layer of bayesian
+        numbers_of_each_type: Dict[str, List[int]]
+            The number of sample size in each type of amino acid
+        adjustment: Dict[str, List[int]]
+            The adjustment to fit gaussian in each type of amino acid
+        grid_points: np.ndarray
+            The distances of grid points to the center of amino acid
+        variances_of_error: Dict[str, List[int]]
+            The variances of errors which is also a gaussian distribution
 
-    def create_data(self) -> np.ndarray:
-        """Create simulated data with given hyper parameters"""
+        Return
+        ----------
+        self.data_log: Dict[str, np.ndarray]
+            The transformed data by log
 
-        for j in range(self.types_of_proteins):
-            data_log_list = []
-            epsilon_list = []
-            beta = np.abs(np.random.normal(self.mu_0, self.sigma_0))
-            self.beta_list.append(beta)
-            self.numbers_of_types = self.numbers_of_each_type[j]
-            for i in range(self.numbers_of_types):
-                A_ij = self.adjustment_list[j][i]
-                sigma_ij = self.variances_of_error[j][i]
-                epsilon = np.random.normal(0, sigma_ij, self.grid_points.shape)
-                A_ij_tilde = -3 / 2 * \
-                    np.log(2 * np.pi * 1 / beta) + np.log(A_ij)
-                data_log = A_ij_tilde + beta * self.X_k + epsilon
-                data_log_list.append(data_log)
-                epsilon_list.append(epsilon)
-            self.epsilon_list.append(epsilon_list)
-            self.data_log_list.append(np.array(data_log_list))
+        """
 
-        return self.data_log_list
+        self.mu_0 = mu_0
+        self.sigma_0 = sigma_0
+        self.numbers_of_each_type = numbers_of_each_type
+        self.adjustment = adjustment
+        self.grid_points = grid_points
+        self.variances_of_error = variances_of_error
+        self.X_k = - 1 / 2 * grid_points**2
+        self.types_of_proteins = len(numbers_of_each_type)
 
+        self.beta = {
+            key: np.abs(
+                np.random.normal(
+                    self.mu_0,
+                    self.sigma_0)) for key in numbers_of_each_type.keys()}
+        self.data_log = {}
+        for type_j, sample_size in numbers_of_each_type.items():
+            beta = self.beta[type_j]
+            epsilon = np.array([np.random.normal(0, variance, grid_points.shape)
+                               for variance in variances_of_error[type_j]])
+            A_ij = adjustment[type_j]
+            A_ij_tilde = -3 / 2 * np.log(2 * np.pi * 1 / beta) + np.log(A_ij)
+            data_list = []
+            for i in range(sample_size):
+                data_list.append(A_ij_tilde[i] + beta * self.X_k + epsilon[i])
+            self.data_log[type_j] = np.array(data_list)
+        return self.data_log
 
-    def caculate_constants(self) -> Tuple[np.ndarray]:
+    def caculate_constants(self) -> Tuple[Dict[str, np.ndarray]]:
         """
         Estimate `A_ij_tilde` by simple linear regression solved by OLS.
-        Caculate the constants of `B_j`, `C_j`, `D_j` before caculate log likelihood function.
+        Estimate `epsilon_ij` by MSE.
+        Caculate the constants of `B_j`, `C_j`, `D_j` `sigma_j_bar` before caculate log likelihood function.
+
+        Params
+        ----------
+
+        Return
+        ----------
+        Tuple[Dict[str, np.ndarray]]
+            The constant values that would be used to caculate the bayes estimator.
         """
-        self.estimated_A_ij_tilde = []
-        self.B_j = np.array([])
-        self.C_j = np.array([])
-        self.D_j = np.array([])
-        self.sigma_j_bar = np.array([])
+        self.estimated_A_ij_tilde = {key: np.array(
+            []) for key in self.numbers_of_each_type.keys()}
+        self.estimated_variances_of_error = {key: np.array(
+            []) for key in self.numbers_of_each_type.keys()}
+        self.B_j = {key: np.array([])
+                    for key in self.numbers_of_each_type.keys()}
+        self.C_j = {key: np.array([])
+                    for key in self.numbers_of_each_type.keys()}
+        self.D_j = {key: np.array([])
+                    for key in self.numbers_of_each_type.keys()}
+        self.sigma_j_bar = {
+            key: np.array(
+                []) for key in self.numbers_of_each_type.keys()}
         X_k_distance = self.X_k.mean()
-        for j in range(self.types_of_proteins):
+        for type_j, sample_size in self.numbers_of_each_type.items():
             estimated_A_ij_tilde_list = []
+            estimated_variance = []
             B_j_list = []
             C_j_list = []
             D_j_list = []
             sigma_j_bar = 1
-            for i in range(self.numbers_of_each_type[j]):
-                cov = np.cov(self.X_k, self.data_log_list[j][i])[0, 1]
+            data_log_arr = self.data_log[type_j]
+            for i in range(sample_size):
+                data_log = data_log_arr[i]
+                cov = np.cov(self.X_k, data_log)[0, 1]
                 var = np.var(self.X_k)
                 beta_ols = cov / var
-                data = self.data_log_list[j][i]
-                A_ij = data.mean() - beta_ols * X_k_distance
+                A_ij = data_log.mean() - beta_ols * X_k_distance
+                variance = sum(
+                    (data_log - (A_ij + self.X_k * beta_ols))**2) / (data_log.shape[0] - 1)
                 estimated_A_ij_tilde_list.append(A_ij)
-                variance = self.variances_of_error[j][i]
+                estimated_variance.append(variance)
                 # Sum `k`
-                B_j_list.append(sum((data - A_ij)**2 / (2 * variance)))
+                B_j_list.append(sum((data_log - A_ij)**2 / (2 * variance)))
                 C_j_list.append(
-                    sum((data - A_ij) * self.grid_points**2 / (2 * variance)))
+                    sum((data_log - A_ij) * self.grid_points**2 / (2 * variance)))
                 D_j_list.append(sum(self.grid_points**4 / (8 * variance)))
                 sigma_j_bar *= variance
-            self.estimated_A_ij_tilde.append(estimated_A_ij_tilde_list)
+            self.estimated_A_ij_tilde[type_j] = np.array(
+                estimated_A_ij_tilde_list)
+            self.estimated_variances_of_error[type_j] = np.array(
+                estimated_variance)
             # Sum `i`
-            self.B_j = np.append(self.B_j, -sum(B_j_list))
-            self.C_j = np.append(self.C_j, -sum(C_j_list))
-            self.D_j = np.append(self.D_j, -sum(D_j_list))
-            self.sigma_j_bar = np.append(self.sigma_j_bar, sigma_j_bar)
+            self.B_j[type_j] = -sum(B_j_list)
+            self.C_j[type_j] = -sum(C_j_list)
+            self.D_j[type_j] = -sum(D_j_list)
+            self.sigma_j_bar[type_j] = sigma_j_bar
+
         return self.estimated_A_ij_tilde, self.B_j, self.C_j, self.D_j, self.sigma_j_bar
 
-    def negative_log_likelihood(self, params: Tuple) -> float:
-        """The negative log likelihood function of `mu_0` and `sigma_0`."""
-        mu_0, sigma_0 = params
-        E_j = 1 - 2 * sigma_0 * self.D_j
-        return -sum(-1 / 2 * np.log(self.sigma_j_bar * E_j) - (sigma_0 * (4 * self.B_j * self.D_j -
-                    self.C_j**2) - 2 * mu_0 * self.C_j - 2 * self.B_j - 2 * mu_0**2 * self.D_j) / (2 * E_j))
+    def negative_log_likelihood(
+            self, params: Tuple[Union[int, float]]) -> float:
+        """
+        The negative log likelihood function of `mu_0` and `sigma_0`.
 
-    def estimate_hyperparameters_by_mle(self) -> Tuple:
-        """Estimate the hyperparameters, `mu_0` and `sigma_0`, of Gaussian distribution by mle."""
+        Params
+        ----------
+        params: Tuple[Union[int, float]]
+            The parameters of negative log-likelihood are `mu` and `sigma`
+
+        Return
+        ----------
+        float
+            The value of negative log-likelihood given params
+        """
+        mu_0, sigma_0 = params
+        B_j = np.array([value for value in self.B_j.values()])
+        C_j = np.array([value for value in self.C_j.values()])
+        D_j = np.array([value for value in self.D_j.values()])
+        sigma_j_bar = np.array([value for value in self.sigma_j_bar.values()])
+        E_j = 1 - 2 * sigma_0 * D_j
+        return -sum(-1 / 2 * np.log(sigma_j_bar * E_j) - (sigma_0 * (4 * B_j * \
+                    D_j - C_j**2) - 2 * mu_0 * C_j - 2 * B_j - 2 * mu_0**2 * D_j) / (2 * E_j))
+
+    def estimate_hyperparameters_by_mle(self) -> Tuple[Union[int, float]]:
+        """
+        Estimate the hyperparameters, `mu_0` and `sigma_0`, of Gaussian distribution by mle.
+
+        Params
+        ----------
+
+        Return
+        ----------
+        Tuple[Union[int, float]]
+            The estimators of `mu_0` and `sigma_0`
+        """
         self.res = minimize(self.negative_log_likelihood,
-                            (-1, 0.1), method='Nelder-Mead')
+                            (2, 0.5), method='Nelder-Mead')
         self.mu_0_hat, self.sigma_0_hat = self.res.x
         return self.mu_0_hat, self.sigma_0_hat
 
-    def estimate_beta_1j_by_Bayse(self) -> np.ndarray:
-        """Estimate `beta_1j` by bayes' estimator."""
-        self.E_j = 1 - 2 * self.sigma_0_hat * self.D_j
-        self.beta_1j_hat = (
-            self.mu_0_hat + self.sigma_0_hat * self.C_j) / self.E_j
+    def estimate_beta_1j_by_Bayse(self) -> Dict[str, np.ndarray]:
+        """
+        Estimate `beta_1j` by bayes' estimator.
+
+        Params
+        ----------
+
+        Return
+        ----------
+        Dict[str, np.ndarray]
+            The bayes estimators of `beta_1j`
+        """
+        C_j = np.array([value for value in self.C_j.values()])
+        D_j = np.array([value for value in self.D_j.values()])
+        E_j = 1 - 2 * self.sigma_0_hat * D_j
+        beta_1j_hat = (self.mu_0_hat + self.sigma_0_hat * C_j) / E_j
+        self.beta_1j_hat = {
+            key: value for key, value in zip(
+                self.C_j.keys(), beta_1j_hat)}
         return self.beta_1j_hat
