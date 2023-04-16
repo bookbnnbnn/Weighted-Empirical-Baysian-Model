cb7e7dcb56a2581475edc21e14876ec0c5579ac9
JamesZheng <jameszheng@mobagel.com>
2023-04-16 15:44:09 +0800

    modify typing and annotations

--- QEB/model.py
+++ QEB/model.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import random
 from scipy.optimize import minimize
-from typing import List, Dict, Tuple, Union
+from typing import List, Dict, Tuple, Optional
 from QEB.utils import *
 
 random.seed(0)
@@ -76,14 +76,44 @@ class QEB:
             self,
             root_map: str,
             root_pdb: str,
-            atomic: str = None,
-            start_rad: Union[int, float] = 0.01,
-            max_rad: Union[int, float] = 1,
-            gap: Union[int, float] = 0.01,
-            max_points: Union[int, float] = 8,
-            base_num_points: Union[int, float] = 4,
-            max_iter: Union[int, float] = 30):
-        """Read data"""
+            atomic: Optional[str] = None,
+            start_rad: float = 0.01,
+            max_rad: float = 1,
+            gap: float = 0.01,
+            max_points: int = 8,
+            base_num_points: int = 4,
+            max_iter: int = 30
+    ) -> Dict[str, np.ndarray]:
+        """
+        Reads map and pdb files, generates grid points, interpolates data, and returns the log values of the
+        interpolated data for each atom type.
+
+        Params
+        ----------
+        root_map: str 
+            Path to the map file.
+        root_pdb: str 
+            Path to the pdb file.
+        atomic: Optional[str]
+            Atom type to filter by. Defaults to None.
+        start_rad: float (default=0.01)
+            Minimum radius for generating grid points. 
+        max_rad: float (default=0.8)
+            Maximum radius for generating grid points. 
+        gap: float (default=0.01)
+            Gap between radii for generating grid points. 
+        max_points: int (default=8)
+            Maximum number of points to generate at each radius. 
+        base_num_points: int (default=4)
+            Number of points to generate at the minimum radius. 
+        max_iter: int (default=30)
+            Maximum number of iterations for generating grid points.
+
+        Return
+        ----------
+        Dict[str, np.ndarray]: 
+            the dictionary containing the log values of the interpolated data for each atom type.
+        """
         data, grid_size, origin = read_map(root_map)
         mean, std = np.mean(data), np.std(data)
         self.A_B = mean + 10 * std, mean - std
@@ -102,6 +132,10 @@ class QEB:
         Estimate `epsilon_ij` by MSE.
         Caculate the constants of `B_j`, `C_j`, `D_j` `sigma_j_bar` before caculate log likelihood function.
 
+        Params
+        ----------
+        None
+        
         Return
         ----------
         Tuple[Dict[str, np.ndarray]]
@@ -154,13 +188,13 @@ class QEB:
         return self.estimated_A_ij_tilde, self.B_j, self.C_j, self.D_j
 
     def negative_log_likelihood(
-            self, params: Tuple[Union[int, float]]) -> float:
+            self, params: Tuple[float]) -> float:
         """
         The negative log likelihood function of `mu_0` and `sigma_0`.
 
         Params
         ----------
-        params: Tuple[Union[int, float]]
+        params: Tuple[float]
             The parameters of negative log-likelihood are `mu` and `sigma`
 
         Return
@@ -176,13 +210,17 @@ class QEB:
         return -sum(-1 / 2 * (np.log(E_j)) - (sigma_0 * (4 * B_j * \
                     D_j - C_j**2) - 2 * mu_0 * C_j - 2 * B_j - 2 * mu_0**2 * D_j) / (2 * E_j))
 
-    def estimate_hyperparameters_by_mle(self) -> Tuple[Union[int, float]]:
+    def estimate_hyperparameters_by_mle(self) -> Tuple[float]:
         """
         Estimate the hyperparameters, `mu_0` and `sigma_0`, of Gaussian distribution by mle.
 
+        Params
+        ----------
+        None
+
         Return
         ----------
-        Tuple[Union[int, float]]
+        Tuple[float]
             The estimators of `mu_0` and `sigma_0`
         """
         self.res = minimize(self.negative_log_likelihood,
@@ -239,14 +277,38 @@ class QEB:
 
     def plot_data(
             self, 
-            amino_acid = None, 
-            indexes = None, 
-            start_rad=0.01,
-            max_rad=0.8,
-            gap=0.01,
-            compared=False,
-            estimated=True
-            ):
+            amino_acid: Optional[str] = None, 
+            indexes: Optional[List[int]] = None, 
+            start_rad: float = 0.01,
+            max_rad: float = 0.8,
+            gap: float = 0.01,
+            compared: bool = False,
+            estimated: bool = True
+        ) -> None:
+        """
+        Plot the density data.
+
+        Params
+        ----------
+        amino_acid: Optional[str]
+            The type of amino acid to plot data for. If None, plot data for all types.
+        indexes: Optional[List[int]]
+            The indexes of the data points to plot. If None, plot all points.
+        start_rad: float
+            The starting radius from the center. 
+        max_rad: float
+            The maximum radius from the center. 
+        gap: float
+            The gap between two radius.
+        compared: bool
+            Whether to compare the estimated Gaussian with the Gaussian in qscore.
+        estimated: bool
+            Whether to plot the estimated Gaussian.
+            
+        Return
+        ----------
+        None
+        """
         self.radius_density, self.estimated_radius_density, self.qscore_radius_density = estimate_density(
             self.grid_points, self.distances_to_center, self.interp_func, self.beta_1j_hat, self.estimated_A_ij_tilde)
         plot_density(self.radius_density, self.estimated_radius_density, self.qscore_radius_density, self.A_B ,
